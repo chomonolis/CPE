e58e0d07b813dc46e9195b2955d6b54b07333a77
chomonolis <tyomo3317@gmail.com>
2022-03-27 00:08:48 +0900

    createUserTeamのhookを作成

--- src/hook/UserTeam.hook.ts
+++ src/hook/UserTeam.hook.ts
@@ -1,8 +1,39 @@
+import { GraphQLResult } from '@aws-amplify/api';
 import { useCallback } from 'react';
 
+import { CreateUserTeamMutation } from '../API';
+import UserTeamService from '../service/userTeam.service';
+
 const useUserTeam = () => {
-  const createUserTeam = useCallback(async () => {}, []);
+  const createUserTeam = useCallback(
+    async (userId: string, teamId: string, userPoint?: number) => {
+      const res = await UserTeamService.createUserTeam({
+        userId: userId,
+        teamId: teamId,
+        userPoint: userPoint,
+      });
+      return res.data?.createUserTeam;
+    },
+    []
+  );
   return { createUserTeam };
+};
+
+export type UseUserTeamReturnType = {
+  createUserTeam: (
+    userId: string,
+    teamId: string,
+    userPoint?: number
+  ) => Promise<
+    Exclude<
+      GraphQLResult<CreateUserTeamMutation>['data'],
+      undefined
+    >['createUserTeam']
+  >;
+  createUserTeamRT: Exclude<
+    GraphQLResult<CreateUserTeamMutation>['data'],
+    undefined
+  >['createUserTeam'];
 };
 
 export default useUserTeam;
