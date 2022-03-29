1a2297eb6c868a60a3e0737b3a4424ccddbd10fd
chomonolis <tyomo3317@gmail.com>
2022-03-29 11:16:01 +0900

    ユーザチームのserviceにgetを追加

--- src/service/userTeam.service.ts
+++ src/service/userTeam.service.ts
@@ -2,7 +2,12 @@ import { API, graphqlOperation } from 'aws-amplify';
 import { GraphQLResult } from '@aws-amplify/api';
 
 import { createUserTeam } from '../graphql/mutations';
-import { CreateUserTeamInput, CreateUserTeamMutation } from '../API';
+import { getUserTeam } from '../graphql/queries';
+import {
+  CreateUserTeamInput,
+  CreateUserTeamMutation,
+  GetUserTeamQuery,
+} from '../API';
 import { PromiseType } from '../utils/typeUtils';
 
 const UserTeamService = {
@@ -11,12 +16,21 @@ const UserTeamService = {
       graphqlOperation(createUserTeam, { input: input })
     ) as Promise<GraphQLResult<CreateUserTeamMutation>>;
   },
+  getUserTeam: async (id: string) => {
+    const result = await API.graphql(graphqlOperation(getUserTeam, { id: id }));
+    if ('data' in result && result.data) {
+      const data = result.data as GetUserTeamQuery;
+      return data.getUserTeam;
+    }
+    return undefined;
+  },
 };
 
 export type UserTeamServiceReturnType = {
   createUserTeamRT: PromiseType<
     ReturnType<typeof UserTeamService.createUserTeam>
   >;
+  getUserTeamRT: PromiseType<ReturnType<typeof UserTeamService.getUserTeam>>;
 };
 
 export default UserTeamService;
