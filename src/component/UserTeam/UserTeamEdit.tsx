4bde1870fed9315b6a41552bfcb3999828cfd055
chomonolis <tyomo3317@gmail.com>
2022-03-29 10:58:48 +0900

    更新バッチ表示の追加

--- src/component/UserTeam/UserTeamEdit.tsx
+++ src/component/UserTeam/UserTeamEdit.tsx
@@ -1,7 +1,9 @@
 import { useState, useEffect } from 'react';
+import { Stack } from '@mui/material';
 
 import useUserTeam from '../../hook/UserTeam.hook';
 import UserEdit from '../User/UserEdit';
+import CustomizedSnackbar from '../CustomizedSnackbar';
 
 type Props = {
   teamId: string;
@@ -13,15 +15,20 @@ const initialUserId = '';
 const UserTeamEdit = (props: Props) => {
   const { teamId, id } = props;
   const [createdUserId, setCreatedUserId] = useState<string>(initialUserId);
+  const [update, setUpdate] = useState<boolean>(false);
+  const [complete, setComplete] = useState<boolean>(false);
   const { createUserTeam } = useUserTeam();
 
   useEffect(() => {
     if (createdUserId === initialUserId) return;
     (async () => {
+      setUpdate(true);
       try {
         const r = await createUserTeam(createdUserId, teamId, 0);
         if (r) {
           setCreatedUserId(initialUserId);
+          setUpdate(false);
+          setComplete(true);
         }
       } catch (err) {
         console.error(err);
@@ -31,7 +38,25 @@ const UserTeamEdit = (props: Props) => {
 
   // idがある -> createではない
   if (id) return <></>;
-  return <UserEdit createFlag setCreatedUserId={setCreatedUserId} />;
+  return (
+    <>
+      <UserEdit createFlag setCreatedUserId={setCreatedUserId} />
+      <Stack spacing={2} sx={{ width: '100%' }}>
+        <CustomizedSnackbar
+          msg="チームとユーザを紐づけています"
+          serverity="info"
+          open={update}
+          setOpen={setUpdate}
+        />
+        <CustomizedSnackbar
+          msg="正常にチームとユーザを紐づけました"
+          serverity="success"
+          open={complete}
+          setOpen={setComplete}
+        />
+      </Stack>
+    </>
+  );
 };
 
 export default UserTeamEdit;
